6148c2b3a7e448b047b17a8b9f08515644b8abde
AleBustamante <adbdc.u@gmail.com>
2024-05-01 17:33:12 -0400

    Creada la clase puntaje y los archivos js para el puntaje por cobertura y sus pruebas

---
+++ src/Puntaje.js
@@ -0,0 +1,7 @@
+export default class Puntaje {
+    constructor() {
+        this.puntajePruebas = [];
+        this.puntajeLineasCodigo = [];
+        this.puntajeCobertura = [];
+    }
+}
---
+++ src/puntaje/moduloPuntCobertura.js
@@ -0,0 +1,1 @@
+import Puntaje from "../Puntaje";
