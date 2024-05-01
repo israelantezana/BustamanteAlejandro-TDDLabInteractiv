8323406bfa7bf5c26f88fe1762c8852990ab123f
AleBustamante <adbdc.u@gmail.com>
2024-05-01 18:36:47 -0400

    Segunda prueba: si la cantidad de cobertura es 0, el puntaje es 0

--- src/puntaje/moduloPuntCobertura.spec.js
+++ src/puntaje/moduloPuntCobertura.spec.js
@@ -4,4 +4,7 @@ describe("calcularPuntajePorCobertura", () => {
   it("Cuando se instancia un objeto Puntaje, todos los puntajes deberían estar en 0", () => {
     expect(puntajePorCobertura(0)).toEqual(0);
   });
+  it("Debería otorgar un puntaje de 0 cuando la cobertura de codigo fue del 0%", () => {
+    expect(puntajePorCobertura(0)).toEqual(0);
+  });
 });
