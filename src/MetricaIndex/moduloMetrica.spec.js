d4fe91e60a6078f210e89d995494771f0061898f
Agustin <agustin.ledezma@ucb.edu.bo>
2024-05-01 11:09:32 -0400

    Se refactorizo la funcion CrearMetrica

--- src/MetricaIndex/moduloMetrica.spec.js
+++ src/MetricaIndex/moduloMetrica.spec.js
@@ -2,11 +2,12 @@ import { crearMetrica } from "./moduloMetrica.js";
 import Metrica from "../Metrica.js"; 
 
 describe("crearMetrica", () => {
-  it("Debería crear una nueva instancia de Metrica con los valores dados", () => {
+  it("Debería crear una nueva instancia de Metrica con los valores dados (no refactor)", () => {
     const metrica = crearMetrica(10, 100, 80);
     expect(metrica).toBeInstanceOf(Metrica);
     expect(metrica.pruebasAñadidas).toBe(10);
     expect(metrica.lineasDeCodigo).toBe(100);
     expect(metrica.cobertura).toBe(80);
+
   });
 });
