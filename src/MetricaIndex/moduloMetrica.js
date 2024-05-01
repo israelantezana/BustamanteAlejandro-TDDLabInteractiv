e9b7ae2ca3a454253829582d27a41f932dc35fc7
Agustin <agustin.ledezma@ucb.edu.bo>
2024-05-01 11:13:09 -0400

    Caso de prueba en la funcion CrearMetrica donde se debería manejar valores nulos o indefinidos

--- src/MetricaIndex/moduloMetrica.js
+++ src/MetricaIndex/moduloMetrica.js
@@ -1,9 +1,16 @@
 import Metrica from "../Metrica.js";
 
 function crearMetrica(pruebasAñadidas, lineasDeCodigo, cobertura) {
-
-  return new Metrica(pruebasAñadidas, lineasDeCodigo, cobertura);
+    if (pruebasAñadidas === null || pruebasAñadidas === undefined) {
+      pruebasAñadidas = 0; 
+    }
+    if (lineasDeCodigo === null || lineasDeCodigo === undefined) {
+      lineasDeCodigo = 0; 
+    }
+    if (cobertura === null || cobertura === undefined) {
+      cobertura = 0; 
+    } 
+    return new Metrica(pruebasAñadidas, lineasDeCodigo, cobertura);
+  }
   
-}
-
 export { crearMetrica };
