1f3ae32df3538a326948d8e4e31293f42d52fe7b
Agustin <agustin.ledezma@ucb.edu.bo>
2024-05-01 10:28:46 -0400

    Se agrega la clase Metrica con sus respectivos atributos

---
+++ src/Metrica.js
@@ -0,0 +1,7 @@
+export default class Metrica {
+    constructor(pruebasAñadidas, lineasDeCodigo, cobertura) {
+        this.pruebasAñadidas = pruebasAñadidas;
+        this.lineasDeCodigo = lineasDeCodigo;
+        this.cobertura = cobertura;
+    }
+}
