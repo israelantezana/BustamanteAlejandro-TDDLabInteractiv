e57447f9e0b1d998e9c2a8bd8406fffc19e1fbbe
Dylan Calle Contreras <dylan.calle@ucb.edu.bo>
2024-05-01 21:30:54 -0400

    Cuando la cantidad de lineas sean menores a 5 o menores a 20, daran 1 punto

--- src/Puntos/Puntos.spec.js
+++ src/Puntos/Puntos.spec.js
@@ -14,7 +14,10 @@ describe("Calcular Puntos", () => {
   it("Debería obtener 3 puntos si las lineas de codigo son entre 5 y 10", () => {
     expect(CalcularPuntosPorLineas(6)).toBe(3);
   });
-  
+  it("Debería obtener 1 puntos si las lineas de codigo son entre 1 y 4 tambien entre 11 y 20", () => {
+    expect(CalcularPuntosPorLineas(15)).toBe(1);
+    expect(CalcularPuntosPorLineas(1)).toBe(1);
+  });
 });
 
 
