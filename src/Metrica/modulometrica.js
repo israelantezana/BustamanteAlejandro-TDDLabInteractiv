8fd0a7ddd94661ebd89abe919fcc29dab87b52a4
alejandra <alejandra.palz@ucb.edu.bo>
2024-05-01 22:17:40 -0400

    se logro refactorizar la funcion crear  metrica

--- src/Metrica/modulometrica.js
+++ src/Metrica/modulometrica.js
@@ -1,7 +1,9 @@
 import Metrica from "../Metrica.js";
 
 function crearMetrica(pruebasAñadidas, lineasDeCodigo, cobertura) {
+
   return new Metrica(pruebasAñadidas, lineasDeCodigo, cobertura);
+  
 }
 
 export { crearMetrica };
--- src/Metrica/modulometrica.spec.js
+++ src/Metrica/modulometrica.spec.js
@@ -1,9 +1,13 @@
-import Metrica from "../Metrica.js";
+import { crearMetrica } from "./modulometrica.js";
+import Metrica from "../Metrica.js"; 
 
-function crearMetrica(pruebasAñadidas, lineasDeCodigo, cobertura) {
+describe("crearMetrica", () => {
+  it("Debería crear una nueva instancia de Metrica con los valores dados (no refactor)", () => {
+    const metrica = crearMetrica(10, 100, 80);
+    expect(metrica).toBeInstanceOf(Metrica);
+    expect(metrica.pruebasAñadidas).toBe(10);
+    expect(metrica.lineasDeCodigo).toBe(100);
+    expect(metrica.cobertura).toBe(80);
 
-  return new Metrica(pruebasAñadidas, lineasDeCodigo, cobertura);
-  
-}
-
-export { crearMetrica };
+  });
+});
