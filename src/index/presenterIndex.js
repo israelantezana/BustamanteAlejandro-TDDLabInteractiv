a3eac8d349673050788584170e652324e2e62a63
Agustin <agustin.ledezma@ucb.edu.bo>
2024-05-02 10:27:09 -0400

    Se implemento el presenter

--- src/index/presenterIndex.js
+++ src/index/presenterIndex.js
@@ -1,10 +1,12 @@
-import {crearProyecto,agregarProyectoAProyectos,mostrarProyectos,eliminarProyecto} from "./moduloIndex.js";
+import {crearProyecto,agregarProyectoAProyectos,mostrarProyectos,eliminarProyecto,buscarProyecto} from "./moduloIndex.js";
 
 const botonAgregarProyecto = document.querySelector("#boton-agregar-proyecto");
 let proyectos = JSON.parse(localStorage.getItem("proyectos")) || [];
 const parametros = new URLSearchParams(window.location.search);
 const titulo = parametros.get("Titulo");
 const descripcion = parametros.get("Descripcion");
+const botonBuscarProyecto = document.querySelector("#boton-buscar-proyecto");
+
 
 const nuevaURL = window.location.pathname;
 window.history.replaceState({}, document.title, nuevaURL);
@@ -29,7 +31,28 @@ proyectos.forEach((proyecto) => {
   proyectosContainer.appendChild(proyectoElement);
 });
 document.body.appendChild(proyectosContainer);
+botonBuscarProyecto.addEventListener("click", function () {
+  const tituloBuscado = inputTitulo.value.trim();
+  
 
+  proyectosContainer.innerHTML = "";
+
+
+  const proyectoEncontrado = buscarProyecto(tituloBuscado, proyectos);
+  
+
+  if (proyectoEncontrado !== null) {
+
+    const proyectoElement = document.createElement("div");
+    proyectoElement.innerHTML = `<h2>${proyectoEncontrado.titulo}</h2><p>${proyectoEncontrado.descripcion}</p>`;
+    proyectosContainer.appendChild(proyectoElement);
+  } else {
+
+    const mensajeElement = document.createElement("p");
+    mensajeElement.textContent = "No se encontró ningún proyecto con el título especificado.";
+    proyectosContainer.appendChild(mensajeElement);
+  }
+});
 botonAgregarProyecto.addEventListener("click", function () {
   window.location.href = "proyectos.html";
 });
